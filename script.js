a6bce8ee9e28af73c77f754f52d33d0adefb4265
Quocs <quochades2000@gmail.com>
2020-08-26 11:58:15 +0700

    Update script.js

--- script.js
+++ script.js
@@ -4,14 +4,36 @@ let doorImage3=document.getElementById('door3');
 
 let botDoorPath='https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/robot.svg';
 let beachDoorPath='https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/beach.svg';
-let spaceDoorPath='./space.svg';
+let spaceDoorPath='https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/space.svg';
+let numClosedDoors=3;
+let openDoor1;
+let openDoor2;
+let openDoor3;
+
+const randomChoreDoorGenerator=()=>{
+  const choreDoor=Math.floor(Math.random()*numClosedDoors);
+  if(choreDoor===0){
+    openDoor1=botDoorPath;
+    openDoor2=beachDoorPath;
+    openDoor3=spaceDoorPath;
+  }else if(choreDoor===1){
+    openDoor2=botDoorPath;
+    openDoor1=beachDoorPath;
+    openDoor3=spaceDoorPath;
+  }else if (choreDoor===2){
+    openDoor3=botDoorPath;
+    openDoor2=spaceDoorPath;
+    openDoor1=beachDoorPath;
+  }
+}
 
 doorImage1.onclick=()=>{
-doorImage1.src= botDoorPath;
+doorImage1.src=openDoor1;
 }
 doorImage2.onclick=()=>{
-doorImage2.src= beachDoorPath;
+doorImage2.src=openDoor2;
 }
-doorImage3.onclick=()=>{
-doorImage3.src = spaceDoorPath;
+doorImage3.onclick= ()=>{
+doorImage3.src=openDoor3;
 }
+randomChoreDoorGenerator();
